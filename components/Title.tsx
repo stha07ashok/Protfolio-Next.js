40712c45cefcfebca2feb0a0d0b5405a5ceb5cdb
Name <user@example.com>
2024-12-28 18:39:31 +0545

    protfolio

--- components/Title.tsx
+++ components/Title.tsx
@@ -4,7 +4,7 @@ interface Props {
   children: React.ReactNode;
   className?: string;
 }
-const Title = ({ children, className }: Props) => {
+const Title = ({ children }: Props) => {
   return (
     <h2 className={cn("text-xl text-lightSky font-bold mb-4")}>{children}</h2>
   );
